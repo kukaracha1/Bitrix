d80b11bbc4adb2510ad37bb8c94b95cd2428e766
Максим Бараников < >
2018-05-11 13:25:10 +0300

    The detail info of goods was complemented by a partner's data.

--- local/templates/.default/components/bitrix/catalog/my_catalog/bitrix/catalog.element/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/catalog/my_catalog/bitrix/catalog.element/.default/result_modifier.php
@@ -7,3 +7,55 @@
 
 $component = $this->getComponent();
 $arParams = $component->applyTemplateModifications();
+
+
+// var_dump($arResult['PROPERTIES']['PARTNER_AT']);
+$partner = $arResult['PROPERTIES']['PARTNER_AT'];
+
+$iblock_id = $partner['LINK_IBLOCK_ID'];
+$element_id = $partner['VALUE'];
+
+// IT WORKS
+//BY DISPLAY_PROPERTIES
+$rsName = CIBlockElement::GetById($element_id);
+while($ob = $rsName->GetNext()) {
+	$VALUE['DISPLAY_VALUE'] =$ob['NAME'];
+	$VALUE['NAME'] =$ob['IBLOCK_NAME'];
+	$VALUE['ID'] = $ob['ID'];
+	$VALUE['CODE'] = $ob['IBLOCK_CODE'];
+	
+	$arResult['DISPLAY_PROPERTIES'][] =$VALUE;
+}
+
+// BY DETAIL_TEXT
+$rsProps = CIBlockElement::GetProperty($iblock_id, $element_id);
+$i=0;
+while($ob = $rsProps->GetNext()) {
+	if ($ob['ID'] != 48)
+	{
+		$VALUE = $ob;
+		$VALUE['DISPLAY_VALUE'] = $ob['VALUE'];
+		$VALUES[] = $VALUE;
+	}
+}
+
+// var_dump($arResult['DETAIL_TEXT']);
+if ($arResult['DETAIL_TEXT_TYPE'] === 'html' )
+{
+	$arResult['DETAIL_TEXT'] .='<p>';
+}
+foreach ($VALUES as $VALUE)
+{
+
+		$arResult['DETAIL_TEXT'] .= '<b>'.$VALUE['NAME'] . ':</b> <br>' . $VALUE['DISPLAY_VALUE'] . '<br>';
+	
+}
+if ($arResult['DETAIL_TEXT_TYPE'] === 'html' )
+{
+	$arResult['DETAIL_TEXT'] .='</p>';
+}
+
+// var_dump($arResult['DETAIL_TEXT']);
+
+
+
