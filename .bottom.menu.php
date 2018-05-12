ca474834768c864ed44914a19a84bc0c6fd84b57
Максим Бараников < >
2018-05-12 12:09:44 +0300

    Was created 'Partner's cabinet' section

--- .bottom.menu.php
+++ .bottom.menu.php
@@ -1,48 +1,53 @@
 <?
 $aMenuLinks = Array(
 	Array(
-		"Новости",
-		"news/",
-		Array(),
-		Array(),
-		""
-	),
-
-	Array(
-		"Как купить",
-		"about/howto/",
-		Array(), 
-		Array(), 
-		"" 
-	),
-
-	Array(
-		"Доставка",
-		"about/delivery/",
+		"Новости", 
+		"news/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
-		"О магазине",
-		"about/",
-		Array(),
-		Array(),
-		""
+		"Как купить", 
+		"about/howto/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Гарантия",
-		"about/guaranty/",
-		Array(),
-		Array(),
-		""
+		"Доставка", 
+		"about/delivery/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Контакты",
-		"about/contacts/",
-		Array(),
-		Array(),
-		""
+		"О магазине", 
+		"about/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
+	Array(
+		"Гарантия", 
+		"about/guaranty/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"about/contacts/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Партнерский кабинет", 
+		"/partnerskiy-kabinet/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
 );
 ?>
---
+++ local/templates/.default/components/bitrix/catalog/personal_cabinet/bitrix/catalog.element/.default/result_modifier.php
@@ -0,0 +1,12 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var CBitrixComponentTemplate $this
+ * @var CatalogElementComponent $component
+ */
+
+$component = $this->getComponent();
+$arParams = $component->applyTemplateModifications();
+
+
+var_dump($USER);
